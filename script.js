0477ae264e93466a75211bbb29a7ac40681d026b
ST10447238 <160851446+ST10447238@users.noreply.github.com>
2024-08-10 16:16:14 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Function to toggle visibility of an element
+function toggleDetails(elementId){
+    var element = document.getElementById(elementId);
+    if (element.style.display ==="none"){
+        element.style.display ="block";
+    }
+    else{
+        element.style.display ="none";
+    }
+}
